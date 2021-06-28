45003db738b208855f0d680a83a34045744f58ae
Vadim Myagchev <Vadim.Myagchev@Amway.com>
2021-06-28 10:57:16 +0300

    up

--- src/test/java/test/homeTests.java
+++ src/test/java/test/homeTests.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package test;
 
 import org.openqa.selenium.By;
@@ -22,15 +21,15 @@ public class homeTests {
         driver.manage().window().maximize();
     }
 
-//    @Test (description = "I Can Win")
-//    public void  pastebinHelloWebDriver()  {
-//         pasteMethods a = new pasteMethods(driver)
-//         .openPage()
-//         .textFormHello()
-//         .expirationTime()
-//         .postFormNameForHello()
-//         .clickToSave();
-//         }
+    @Test (description = "I Can Win")
+    public void  pastebinHelloWebDriver()  {
+         pasteMethods a = new pasteMethods(driver)
+         .openPage()
+         .textFormHello()
+         .expirationTime()
+         .postFormNameForHello()
+         .clickToSave();
+         }
 
     @Test (description = "Bring it on")
     public void  pastebinVerifyAssertionWebDriver() throws InterruptedException {
@@ -41,93 +40,26 @@ public class homeTests {
          .expirationTime()
          .postFormNameForCode()
          .clickToSave();
-    //     .verifyTitle();
+//         .verifyTitle();
+
         new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.className("info-top")));
         String actualTitle = driver.findElement(By.xpath("//*[@class='info-top']/h1")).getText();
-        String actualText = driver.findElement(By.xpath("//*[@class='info-top']/h1")).getText();
-
-        String colorString = driver.findElement(By.id("foo")).getAttribute("class");
-        String[] arrColor = colorString .split("#");
-        assertTrue(arrColor[1].equals("FFFFFF"));
-
         String expectedTitle="how to gain dominance among developers";
+
+        String actualText = driver.findElement(By.className("de1")).getText();
         String expectedText="git config --global user.name  \"New Sheriff in Town\"\ngit reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\ngit push origin master --force";
+
+        String actualColor = driver.findElement(By.xpath("//*[@class='de1']/*[@class='kw2'])[1]")).getCssValue("color");
+        String expectedColor="#c20cb9";
 
         Assert.assertTrue(actualTitle.contains (expectedTitle), "Page title is not correct");
         Assert.assertTrue(actualText.contains (expectedText), "Page text is not correct");
         Assert.assertTrue(actualColor.contains (expectedColor), "Text color is not correct");
     }
 
-//    @AfterMethod (alwaysRun = true)
-//    public void browserClosure() {
-//         driver.quit();
-//         driver=null;
-//    }
-}
-=======
-package test;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import page.pasteMethods;
-
-public class homeTests {
-
-    private WebDriver driver;
-
-    @BeforeMethod (alwaysRun = true)
-    public void browserSetup (){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    @AfterMethod (alwaysRun = true)
+    public void browserClosure() {
+         driver.quit();
+         driver=null;
     }
-
-//    @Test (description = "I Can Win")
-//    public void  pastebinHelloWebDriver()  {
-//         pasteMethods a = new pasteMethods(driver)
-//         .openPage()
-//         .textFormHello()
-//         .expirationTime()
-//         .postFormNameForHello()
-//         .clickToSave();
-//         }
-
-    @Test (description = "Bring it on")
-    public void  pastebinVerifyAssertionWebDriver() throws InterruptedException {
-          pasteMethods a = new pasteMethods(driver)
-         .openPage()
-         .textFormCode()
-         .syntaxHighlight()
-         .expirationTime()
-         .postFormNameForCode()
-         .clickToSave();
-    //     .verifyTitle();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.className("info-top")));
-        String actualTitle = driver.findElement(By.xpath("//*[@class='info-top']/h1")).getText();
-        String actualText = driver.findElement(By.xpath("//*[@class='info-top']/h1")).getText();
-
-        String colorString = driver.findElement(By.id("foo")).getAttribute("class");
-        String[] arrColor = colorString .split("#");
-        assertTrue(arrColor[1].equals("FFFFFF"));
-
-        String expectedTitle="how to gain dominance among developers";
-        String expectedText="git config --global user.name  \"New Sheriff in Town\"\ngit reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\ngit push origin master --force";
-
-        Assert.assertTrue(actualTitle.contains (expectedTitle), "Page title is not correct");
-        Assert.assertTrue(actualText.contains (expectedText), "Page text is not correct");
-        Assert.assertTrue(actualColor.contains (expectedColor), "Text color is not correct");
-    }
-
-//    @AfterMethod (alwaysRun = true)
-//    public void browserClosure() {
-//         driver.quit();
-//         driver=null;
-//    }
 }
->>>>>>> 88e9c74dced5e3021301544bf60637b85cdf9fc6
